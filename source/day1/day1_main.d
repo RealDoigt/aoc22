adf5c098694f522bf18fa372ee835dab67cc831f
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-01 20:40:00 -0500

    Update day1_main.d

--- source/day1/day1_main.d
+++ source/day1/day1_main.d
@@ -1,14 +1,15 @@
 module day1_main;
+import std.std.algorithm.mutation;
 import std.string;
 import std.stdio;
 import std.file;
 import day1;
 
-void day1Main()
+auto findMostCalories(Elf*[] elves)
 {
     ulong mostCalories, index;
 
-    foreach(i, elf; "res/day1/input".readText.makeElves)
+    foreach(i, elf; elves)
         if (elf.total > mostCalories)
         {
             mostCalories = elf.total;
@@ -16,4 +17,19 @@ void day1Main()
         }
 
     "Elf #%d has the %d calories!".format(index, mostCalories).writeln;
+
+    return i;
+}
+
+void day1Main()
+{
+    auto elves = "res/day1/input".readText.makeElves,
+    ulong totalTop3, index;
+
+    for (int i; i < 3; ++i)
+    {
+        index = elves.findMostCalories;
+        totalTop3 = elves[index].total;
+        elves.remove(index);
+    }
 }
