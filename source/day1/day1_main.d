24f1c7b5eac2984efca8dceb3270964152f41256
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-01 19:59:54 -0500

    Update day1_main.d

--- source/day1/day1_main.d
+++ source/day1/day1_main.d
@@ -1,6 +1,18 @@
 module day1_main;
+import std.stdio;
+import std.file;
+import day1;
 
 void day1_main()
 {
+    auto mostCalories = 0UL, index = -1;
 
+    foreach(i, elf; "res/day1/input".readText.makeElves)
+        if (elf.total > mostCalories)
+        {
+            mostCalories = elf.total;
+            index = -1;
+        }
+
+    "Elf #%d has the %d calories!".format(index, mostCalories);
 }
