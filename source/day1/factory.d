c807e066ef44da5478071ca479ee89848dc4323c
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-01 19:10:59 -0500

    Update factory.d

--- source/day1/factory.d
+++ source/day1/factory.d
@@ -1,9 +1,25 @@
 module day1.factory;
+import std.array;
+import std.conv;
 import day1.elf;
 
-auto makeElves(string s)
+
+auto makeElves(string inventory)
 {
     Elf[] elves;
+    ulong[] calories;
+
+    foreach(line; inventory.split("\n"))
+    {
+        if(line == "")
+        {
+            elves ~= new Elf(calories);
+            calories.clear;
+            continue;
+        }
+
+        calories ~= line.to!ulong;
+    }
 
     return elves;
 }
