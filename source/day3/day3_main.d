ac0cdf2bb3cde224e1ff9bdca34080378e4039bf
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-03 12:07:23 -0500

    Update day3_main.d

--- source/day3/day3_main.d
+++ source/day3/day3_main.d
@@ -5,15 +5,20 @@ import day3;
 
 void day3Main()
 {
-    uint[] commonPriorities;
-    uint[] badges;
+    uint[] commonPriorities,
+           inventories,
+           badges;
 
     foreach(line; "res/day3/input".readText.split("\n"))
     {
         if (line.length == 0) continue;
         auto priorities = line.toPriorities;
         commonPriorities ~= priorities.toCompartments.findCommonPriority;
+
+        inventories ~= priorities;
     }
+
+
 
     "The priorities total is %d".format(commonPriorities.sum).writeln;
 }
