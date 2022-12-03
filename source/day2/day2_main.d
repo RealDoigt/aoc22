ef0a78d12c48120a2de2eafa2c8248a6dcb933cb
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-02 21:40:26 -0500

    Update day2_main.d

--- source/day2/day2_main.d
+++ source/day2/day2_main.d
@@ -7,12 +7,17 @@ import day2;
 public void day2Main()
 {
     uint total;
+    auto lines = "res/day2/input".readText.split("\n");
 
-    foreach(line; "res/day2/input".readText.split("\n"))
+    foreach(line; lines)
     {
         if (line.length == 0) continue;
         total += Game(line[2].getHand, line[0].getHand).score;
     }
 
-    "The total score is %d".format(total).writeln;
+    "The total score is %d for the first strategy".format(total).writeln;
+
+    total = 0;
+
+
 }
