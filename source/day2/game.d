c15315b2e757619e18e2d36bcac525caa973c6af
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-02 21:46:22 -0500

    Update game.d

--- source/day2/game.d
+++ source/day2/game.d
@@ -11,6 +11,16 @@ struct Game
         this.elf = elf;
     }
 
+    this (Result expected, Hand elf)
+    {
+        switch (expected)
+        {
+            case Result.loss: player = elf.loseAgainst;
+            case Result.win: player = elf.winAgainst;
+            default: player = elf;
+        }
+    }
+
     auto score()
     {
         return player + getResult(player, elf);
