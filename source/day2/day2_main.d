320a10d59cd3584d45f36a38a0ac5df0e78b34ae
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-02 21:24:07 -0500

    Update day2_main.d

--- source/day2/day2_main.d
+++ source/day2/day2_main.d
@@ -9,5 +9,8 @@ public void day2Main()
     uint total;
 
     foreach(line; "res/day2/input".readText.split("\n"))
+    {
+        if (line.length == 0) break;
         total += Game(line[2].getHand, line[0].getHand).score;
+    }
 }
