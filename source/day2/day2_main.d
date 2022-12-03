1f53711f3fd931429b43ffbcb5c4426ca0261fbb
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-02 21:12:52 -0500

    Update day2_main.d

--- source/day2/day2_main.d
+++ source/day2/day2_main.d
@@ -1,3 +1,13 @@
+module day2.main;
+import std.string;
+import std.stdio;
+import std.file;
+import day2;
+
 public void day2Main()
 {
+    uint total;
+
+    foreach(line; "res/day2/input".readText.split("\n"))
+        total += Game(line[2].getHand, line[0].getHand).score;
 }
