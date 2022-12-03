a9a99b5f65a9d9ccdc262c6c19d67ba72c6c2337
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-03 09:28:30 -0500

    Create lists.d

---
+++ source/day3/lists.d
@@ -0,0 +1,16 @@
+module day3.lists;
+
+auto toPriorities(char[] rucksack)
+{
+    ubyte[] result;
+
+    foreach (r; rucksack)
+    {
+    }
+
+    return result;
+}
+
+auto toCompartments(ubyte[] priorities)
+{
+}
