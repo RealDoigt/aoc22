9f4907226018aa7c0b5cd4cce5f6b277db20bcda
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-03 09:18:40 -0500

    Update app.d

--- source/app.d
+++ source/app.d
@@ -4,11 +4,12 @@ import std.algorithm.searching;
 
 void main(string[] args)
 {
-    if (args.length == 1) day2Main;
+    if (args.length == 1) day3Main;
 
     else
     {
         if (args.canFind("1")) day1Main;
         if (args.canFind("2")) day2Main;
+        if (args.canFind("3")) day3Main;
     }
 }
