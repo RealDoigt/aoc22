be7b9cde0e144e24e75698495443f1a75f3fa781
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-03 09:38:49 -0500

    Update lists.d

--- source/day3/lists.d
+++ source/day3/lists.d
@@ -1,4 +1,11 @@
 module day3.lists;
+import std.ascii;
+
+auto toPriority(char item)
+{
+    auto u = cast(ubyte)item;
+    return item.isLower ? u - 96 : u - 38;
+}
 
 auto toPriorities(char[] rucksack)
 {
@@ -6,6 +13,7 @@ auto toPriorities(char[] rucksack)
 
     foreach (r; rucksack)
     {
+
     }
 
     return result;
