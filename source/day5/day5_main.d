1b4aae0a49cde19a4dfd06939ad609f983b79581
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-05 00:43:22 -0500

    Update day5_main.d

--- source/day5/day5_main.d
+++ source/day5/day5_main.d
@@ -1,6 +1,11 @@
 module day5.main;
+import common;
 
 void day5Main()
 {
+    char[8][56] stacks;
 
+    foreach (i, line; "res/day5/stacks".readText.split("\n"))
+        foreach (j, c; line)
+            stacks[i][j] = c;
 }
