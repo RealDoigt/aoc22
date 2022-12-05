85ff8ed5292f943da94f86680ae83f0e5456c8e2
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-05 01:13:54 -0500

    Update stacks.d

--- source/day5/stacks.d
+++ source/day5/stacks.d
@@ -10,3 +10,23 @@ void print(char[8][56] stacks)
         writeln;
     }
 }
+
+auto isOccupied(ref char[8][56] stacks, int x, y)
+{
+    return stacks[y][x] >= 'A' && stacks[y][x] <= 'Z';
+}
+
+void moveCrate(ref char[8][56] stacks, int from, int to)
+{
+    char toMove;
+
+    for(int i;;++i)
+        if (i == 55 || !stacks.isOccupied(from, i + 1))
+        {
+            toMove = stacks[from, i];
+            stacks[from, i] = '\0';
+            break;
+        }
+
+
+}
