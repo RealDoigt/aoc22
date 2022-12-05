953e25929ea5b896ca14822e2e40cd600272460a
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-12-05 00:55:15 -0500

    Update day5_main.d

--- source/day5/day5_main.d
+++ source/day5/day5_main.d
@@ -1,6 +1,17 @@
 module day5.main;
 import common;
 
+void print(char[8][56] stacks)
+{
+    for (int i; i < 9; ++i)
+    {
+        foreach(crate; stacks[i])
+            if (crate <= 'Z') crate.write;
+
+        writeln;
+    }
+}
+
 void day5Main()
 {
     char[8][56] stacks;
@@ -8,4 +19,6 @@ void day5Main()
     foreach (i, line; "res/day5/stacks".readText.split("\n"))
         foreach (j, c; line)
             stacks[i][j] = c;
+
+    stacks.print;
 }
